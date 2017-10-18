7dd83604e89ec45a629a0f8d8c37c78c82047df7
Pritish Sai <Pritish.sai@gmail.com>
2017-10-18 04:30:12 -0400

    created the show page for users and added some text to banner image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
 devise :database_authenticatable, :registerable,
        :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+ 
+protected
+	def confirmation_required?
+      false
+    end
+
+has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/assets/images/user.png"
+validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011202445) do
+ActiveRecord::Schema.define(version: 20171018075546) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +54,10 @@ ActiveRecord::Schema.define(version: 20171011202445) do
     t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
